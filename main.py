81a21e75c2e7ec268dd3ef0cb73ed78fbbf52b34
ElvisAvotins <124890311+ElvisAvotins@users.noreply.github.com>
2023-03-19 19:51:59 +0000

    commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 def parallel_processing(n, m, data):
     output = [(0, i) for i in range(n)]
     result = []
-    for i in range(m):
-        ti = data[i]
-        finish_time, output_index = output[0]
+    for ti in data:
+        finish_time, output_index = min(output)
         result.append((output_index, finish_time))
-        output[0] = (finish_time + ti, output_index)
-        j = 0
-        while j < n-1 and output[j][0] > output[j+1][0]:
-            output[j], output[j+1] = output[j+1], output[j]
-            j += 1
+        output[output_index] = (finish_time + ti, output_index)
     return result
-
 def main():
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
