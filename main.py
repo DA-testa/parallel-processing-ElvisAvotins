d78f4afa7c94b1e775887b303008a44800be68f8
ElvisAvotins <124890311+ElvisAvotins@users.noreply.github.com>
2023-03-19 20:03:18 +0000

    Complete

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def parallel_processing(n, m, data):
         result.append((output_index, finish_time))
         output[output_index] = (finish_time + ti, output_index)
     return result
+
 def main():
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
     result = parallel_processing(n,m,data)
     for i in range(m):
         print(result[i][0], result[i][1])
-
+        
 if __name__ == "__main__":
     main()
